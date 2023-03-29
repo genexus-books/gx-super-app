fca4262db752b841f0519951a3e50391f48ed16d
Fabian Inthamoussu <finthamoussu@genexus.com>
2023-03-29 19:23:32 +0200

    Update Flutter example for iOS

---
+++ Flutter/ExampleSuperApp/ios/Classes/ProvisioningAPI.swift
@@ -0,0 +1,161 @@
+import Flutter
+import UIKit
+import GXCoreBL
+
+class ProvisioningAPI {
+	public func handleMethodCall(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+		switch call.method {
+		case Constants.Methods.GET_MINI_APPS:
+			let tag = (call.arguments as? [String: Any])?[Constants.Arguments.TAG] as? String ?? ""
+			getMiniApps(tag: tag, result: result)
+		case Constants.Methods.GET_MINI_APPS_CACHED:
+			getCachedMiniApps(result: result)
+		case Constants.Methods.REMOVE:
+			guard let miniAppJson = (call.arguments as? [String: Any])?[Constants.Arguments.MINI_APP_JSON] as? String,
+				  !miniAppJson.isEmpty, let miniAppJsonData = miniAppJson.data(using: .utf8),
+				  let miniAppInfo = try? JSONSerialization.jsonObject(with: miniAppJsonData) as? [String: Any] else {
+				let error = FlutterError(code: Constants.Errors.ARGUMENTS, message: "MiniApp json cannot be null or empty", details: nil)
+				result(error)
+				return
+			}
+			guard let id = miniAppInfo[Constants.MiniAppFields.ID] as? String else {
+				let error = FlutterError(code: Constants.Errors.ARGUMENTS, message: "Cannot delete MiniApp as it contains incorrect information", details: nil)
+				result(error)
+				return
+			}
+			let version = miniAppInfo[Constants.MiniAppFields.VERSION] as? Int
+			remove(id: id, version: version, result: result)
+		case Constants.Methods.LOAD:
+			guard let miniAppJson = (call.arguments as? [String: Any])?[Constants.Arguments.MINI_APP_JSON] as? String,
+				  !miniAppJson.isEmpty, let miniAppJsonData = miniAppJson.data(using: .utf8),
+				  let miniAppInfo = try? JSONDecoder().decode(GXMiniAppInformation.self, from: miniAppJsonData) else {
+				let error = FlutterError(code: Constants.Errors.ARGUMENTS, message: "MiniApp json cannot be null or empty", details: nil)
+				result(error)
+				return
+			}
+			load(miniApp: miniAppInfo, result: result)
+			break
+		default:
+			break
+		}
+	}
+	
+	private func getMiniApps(tag: String, result: @escaping FlutterResult) {
+		GXSuperAppProvisioning.miniAppsInfoByTag(tag, start: 0, count: 10) {
+			switch $0 {
+			case .failure(let error):
+				let flutterError = FlutterError(code: Constants.Errors.GET,
+												message: "Couldn't retrieve MiniApps from server, cause '\(error)'",
+												details: nil)
+				result(flutterError)
+			case .success(let miniAppsInfo):
+				guard let miniAppsInfoJSONData = try? JSONEncoder().encode(miniAppsInfo),
+					  let miniAppsInfoJSONString = String(data: miniAppsInfoJSONData, encoding: .utf8) else {
+					let flutterError = FlutterError(code: Constants.Errors.GET,
+													message: "Couldn't encode MiniApps to json",
+													details: nil)
+					result(flutterError)
+					return
+				}
+				result(miniAppsInfoJSONString)
+			}
+		}
+	}
+	
+	private func getCachedMiniApps(result: @escaping FlutterResult) {
+		DispatchQueue.gxTryOnBackground(execute: {
+			return try GXMiniAppsManager.cachedMiniApps()
+		}, callback: {
+			switch $0 {
+			case .failure(let error):
+				let flutterError = FlutterError(code: Constants.Errors.GET,
+												message: "Couldn't retrieve cached MiniApps from server, cause '\(error)'",
+												details: nil)
+				result(flutterError)
+			case .success(let cachedMiniApps):
+				let encoder = JSONEncoder()
+				encoder.dateEncodingStrategy = .iso8601
+				encoder.keyEncodingStrategy = .custom({ codingPath in
+					guard codingPath.count > 0 else {
+						fatalError("Invalid coding key")
+					}
+					let key = codingPath.last!
+					enum MappedCodingKeys: String, CodingKey {
+						case miniAppId = "Id"
+						case miniAppVersion = "Version"
+					}
+					switch key.stringValue {
+					case "MiniAppId":
+						return MappedCodingKeys.miniAppId
+					case "MiniAppVersion":
+						return MappedCodingKeys.miniAppVersion
+					default:
+						return key
+					}
+				})
+				guard let cachedMiniAppsJSONData = try? encoder.encode(cachedMiniApps),
+					  let cachedMiniAppsJSONString = String(data: cachedMiniAppsJSONData, encoding: .utf8) else {
+					let flutterError = FlutterError(code: Constants.Errors.GET,
+														 message: "Couldn't encode cached MiniApps to json",
+														 details: nil)
+					result(flutterError)
+					return
+				}
+				result(cachedMiniAppsJSONString)
+			}
+		})
+	}
+	
+	private func load(miniApp: GXMiniAppInformation, result: @escaping FlutterResult) {
+		GXMiniAppsManager.loadMiniApp(info: miniApp) { error in
+			if let error = error {
+				let flutterError = FlutterError(code: Constants.Errors.LOAD,
+												message: "Couldn't load MiniApp '\(miniApp.id)', cause '\((error as NSError).localizedDescription)'",
+												details: nil)
+				result(flutterError)
+			}
+			else {
+				result(true)
+			}
+		}
+	}
+	
+	private func remove(id: String, version: Int?, result: @escaping FlutterResult) {
+		DispatchQueue.gxTryOnBackground(execute: {
+			return try GXMiniAppsManager.removeCachedMiniApp(id: id, version: version)
+		}, callback: {
+			switch $0 {
+			case .failure(let error):
+				let flutterError = FlutterError(code: Constants.Errors.GET,
+												message: "Couldn't retrieve cached MiniApps from server, cause '\(error)'",
+												details: nil)
+				result(flutterError)
+			case .success(let deleted):
+				result(deleted)
+			}
+		})
+	}
+	
+	private struct Constants {
+		static let CHANNEL_NAME = "example_superapp"
+		struct Methods {
+			static let GET_MINI_APPS = "getMiniApps"
+			static let GET_MINI_APPS_CACHED = "getCachedMiniApps"
+			static let LOAD = "load"
+			static let REMOVE = "remove"
+		}
+		struct Arguments {
+			static let TAG = "tag"
+			static let MINI_APP_JSON = "miniAppJson"
+		}
+		struct Errors {
+			static let ARGUMENTS = "1"
+			static let GET = "2"
+			static let LOAD = "3"
+		}
+		struct MiniAppFields {
+			static let ID = "Id"
+			static let VERSION = "Version"
+		}
+	}
+}
---
+++ Flutter/ExampleSuperApp/ios/Classes/SampleExternalObject/SampleExObjHandler.swift
@@ -0,0 +1,212 @@
+//
+//  SampleExObjHandler.swift
+//  ExampleSuperApp
+//
+
+import Flutter
+import GXCoreBL
+
+@objc public class SampleExObjHandler: GXActionExternalObjectHandler {
+	
+	public override static func handleActionExecutionUsingMethodHandlerSelectorNamePrefix() -> Bool {
+		return true
+	}
+	
+	// MARK: Action Handlers
+	
+	@objc public func gxActionExObjMethodHandler_PayWithUI(_ parameters: [Any]) {
+		if let error = validateNumber(ofParametersReceived: UInt(parameters.count), expected: 1) {
+			onFinishedExecutingWithError(error)
+			return
+		}
+		let amount: Double
+		let presentingController: UIViewController
+		do {
+			try amount = paymentAmout(from: parameters[0])
+			try presentingController = presentingViewController()
+		}
+		catch {
+			onFinishedExecutingWithError(error)
+			return
+		}
+		let args = ["amount": Int(amount)]
+		SuperAppAPI.callFlutterUIMethod(name: Constants.Methods.PAY_UI, arguments: args, uiContext: presentingController) { [weak self] result in
+			guard let sself = self else { return }
+			guard let returnValue = result as? String else {
+				let error: NSError
+				if let flutterError = result as? FlutterError {
+					error = NSError.defaultGXError(withDeveloperDescription: flutterError.description)
+				}
+				else {
+					error = NSError.defaultGXError()
+				}
+				sself.onFinishedExecutingWithError(error)
+				return
+			}
+			sself.setReturnValue(returnValue)
+			sself.onFinishedExecutingWithSuccess()
+		}
+	}
+	
+	@objc public func gxActionExObjMethodHandler_PayWithoutUI(_ parameters: [Any]) {
+		if let error = validateNumber(ofParametersReceived: UInt(parameters.count), expected: 1) {
+			onFinishedExecutingWithError(error)
+			return
+		}
+		let amount: Double
+		do {
+			try amount = paymentAmout(from: parameters[0])
+		}
+		catch {
+			onFinishedExecutingWithError(error)
+			return
+		}
+		let args = ["amount": Int(amount)]
+		SuperAppAPI.callMethod(name: Constants.Methods.PAY_NO_UI, arguments: args) { [weak self] result in
+			guard let sself = self else { return }
+			guard let returnValue = result as? String else {
+				let error: NSError
+				if let flutterError = result as? FlutterError {
+					error = NSError.defaultGXError(withDeveloperDescription: flutterError.description)
+				}
+				else {
+					error = NSError.defaultGXError()
+				}
+				sself.onFinishedExecutingWithError(error)
+				return
+			}
+			sself.setReturnValue(returnValue)
+			sself.onFinishedExecutingWithSuccess()
+		}
+	}
+	
+	/// Method that receives a String and returns a Client SDT
+	@objc public func gxActionExObjMethodHandler_GetClientInformation(_ parameters: [Any]) {
+		if let error = validateNumber(ofParametersReceived: UInt(parameters.count), expected: 1) {
+			onFinishedExecutingWithError(error)
+			return
+		}
+		guard let clientID = stringParameter(actionDescParametersArray![0], fromValue: parameters[0]), !clientID.isEmpty else {
+			let error = invalidParameterErrorValid(forMethod: nil, at: 0, developerDescription: "The Client ID must be not empty.")
+			onFinishedExecutingWithError(error)
+			return
+		}
+		let args = ["clientId": clientID]
+		SuperAppAPI.callMethod(name: Constants.Methods.GET_CLIENT_INFO, arguments: args) { [weak self] result in
+			guard let sself = self else { return }
+			guard let returnValue = result as? [String: AnyObject] else {
+				let error: NSError
+				if let flutterError = result as? FlutterError {
+					error = NSError.defaultGXError(withDeveloperDescription: flutterError.description)
+				}
+				else {
+					error = NSError.defaultGXError()
+				}
+				sself.onFinishedExecutingWithError(error)
+				return
+			}
+			let clientSDTTypeName: String = "Client" // as defined in GeneXus API for mini-apps
+			let clientSDTReturnValue = GXSDTData.fromStructureDataTypeName(clientSDTTypeName,
+																		   modelObject: sself.actionDesc,
+																		   value: returnValue,
+																		   fieldIsCollection: false)
+			sself.setReturnValue(clientSDTReturnValue)
+			sself.onFinishedExecutingWithSuccess()
+		}
+	}
+	
+	/// Method that receives a Client SDT and returns a PaymentInformation SDT (Collection)
+	@objc public func gxActionExObjMethodHandler_GetPaymentInformation(_ parameters: [Any]) {
+		if let error = validateNumber(ofParametersReceived: UInt(parameters.count), expected: 1) {
+			onFinishedExecutingWithError(error)
+			return
+		}
+		guard let clientSDT = sdtParameter(actionDescParametersArray![0], fromValue: parameters[0]),
+			  let clientJSONObj = clientSDT.json else {
+			let error = invalidParameterErrorValid(forMethod: nil, at: 0, developerDescription: "Invalid Client parameter.")
+			onFinishedExecutingWithError(error)
+			return
+		}
+		let args = ["clientInfo": clientJSONObj]
+		SuperAppAPI.callMethod(name: Constants.Methods.PAYMENT_INFORMATION, arguments: args) { [weak self] result in
+			guard let sself = self else { return }
+			guard let returnValue = result as? [String: AnyObject] else {
+				let error: NSError
+				if let flutterError = result as? FlutterError {
+					error = NSError.defaultGXError(withDeveloperDescription: flutterError.description)
+				}
+				else {
+					error = NSError.defaultGXError()
+				}
+				sself.onFinishedExecutingWithError(error)
+				return
+			}
+			let paymentInformationSDTTypeName: String = "PaymentInformation" // as defined in GeneXus API for mini-apps
+			let paymentInformationSDTReturnValue = GXSDTDataCollection.fromStructureDataTypeName(paymentInformationSDTTypeName,
+																								 modelObject: sself.actionDesc,
+																								 value: returnValue,
+																								 fieldIsCollection: false)
+			sself.setReturnValue(paymentInformationSDTReturnValue)
+			sself.onFinishedExecutingWithSuccess()
+		}
+	}
+	
+	/// Method that receives a PaymentInformation SDT (Collection) and returns a String
+	@objc public func gxActionExObjMethodHandler_GetPaymentInfoAffinity(_ parameters: [Any]) {
+		if let error = validateNumber(ofParametersReceived: UInt(parameters.count), expected: 1) {
+			onFinishedExecutingWithError(error)
+			return
+		}
+		guard let paymentInfoSDT = sdtCollectionParameter(actionDescParametersArray![0], fromValue: parameters[0]),
+			  paymentInfoSDT.sdtDataCollectionItemsCount > 0,
+			  let paymentInfoJSONObj = paymentInfoSDT.json else {
+			let error = invalidParameterErrorValid(forMethod: nil, at: 0, developerDescription: "Invalid Payment Information parameter.")
+			onFinishedExecutingWithError(error)
+			return
+		}
+		let args = ["paymentInfo": paymentInfoJSONObj]
+		SuperAppAPI.callMethod(name: Constants.Methods.GET_PAYMENT_AFFINITY, arguments: args) { [weak self] result in
+			guard let sself = self else { return }
+			guard let returnValue = result as? String else {
+				let error: NSError
+				if let flutterError = result as? FlutterError {
+					error = NSError.defaultGXError(withDeveloperDescription: flutterError.description)
+				}
+				else {
+					error = NSError.defaultGXError()
+				}
+				sself.onFinishedExecutingWithError(error)
+				return
+			}
+			sself.setReturnValue(returnValue)
+			sself.onFinishedExecutingWithSuccess()
+		}
+	}
+	
+	// MARK: Private
+	
+	private struct Constants {
+		struct Methods {
+			static let PAY_NO_UI = "PayWithoutUI"
+			static let PAY_UI = "PayWithUI"
+			static let GET_CLIENT_INFO = "GetClientInformation"
+			static let PAYMENT_INFORMATION = "GetPaymentInformation"
+			static let GET_PAYMENT_AFFINITY = "GetPaymentInfoAffinity"
+		}
+	}
+
+	private func paymentAmout(from parameter: Any) throws -> Double {
+		guard let amount = (parameter as? NSNumber)?.doubleValue, amount > 0 else {
+			throw NSError.defaultGXError(withLocalizedDescription: "The amount must be greater than 0.")
+		}
+		return amount
+	}
+
+	private func presentingViewController() throws -> UIViewController {
+		guard let presentingController = gxActionHandlerUserInterfaceController?.actionHandlerUserInterfaceController,
+			  presentingController.presentedViewController == nil else {
+			throw NSError.fatalGXError(withDeveloperDescription: "No valid prensenting view controller found for payment UI.")
+		}
+		return presentingController
+	}
+}
