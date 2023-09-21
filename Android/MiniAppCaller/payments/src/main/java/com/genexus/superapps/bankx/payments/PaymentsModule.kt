d49fd303ec59b1f5c6801df59071259c47bc4bb7
Juan Pablo González <jotapeg@users.noreply.github.com>
2023-09-21 10:57:48 -0300

    [Android] Use new Super App API setting syntax (#16)

--- Android/MiniAppCaller/payments/src/main/java/com/genexus/superapps/bankx/payments/PaymentsModule.kt
+++ Android/MiniAppCaller/payments/src/main/java/com/genexus/superapps/bankx/payments/PaymentsModule.kt
@@ -1,12 +1,14 @@
 package com.genexus.superapps.bankx.payments
 
 import android.content.Context
+import com.genexus.android.core.base.metadata.superapp.api.SuperAppApi
+import com.genexus.android.core.base.services.Services
 import com.genexus.android.core.framework.GenexusModule
-import com.genexus.android.core.externalapi.ExternalApiFactory
 import com.genexus.android.core.externalapi.ExternalApiDefinition
 
 class PaymentsModule : GenexusModule {
     override fun initialize(context: Context) {
-        ExternalApiFactory.addApi(ExternalApiDefinition(PaymentsApi.NAME, PaymentsApi::class.java, false))
+        val def = ExternalApiDefinition(PaymentsApi.NAME, PaymentsApi::class.java, false)
+        Services.SuperApps.Api = SuperAppApi(def)
     }
 }
--- Flutter/example_superapp/android/src/main/kotlin/com/genexus/example_superapp/api/PaymentsModule.kt
+++ Flutter/example_superapp/android/src/main/kotlin/com/genexus/example_superapp/api/PaymentsModule.kt
@@ -1,12 +1,14 @@
 package com.genexus.example_superapp.api
 
 import android.content.Context
+import com.genexus.android.core.base.metadata.superapp.api.SuperAppApi
+import com.genexus.android.core.base.services.Services
 import com.genexus.android.core.externalapi.ExternalApiDefinition
-import com.genexus.android.core.externalapi.ExternalApiFactory
 import com.genexus.android.core.framework.GenexusModule
 
 class PaymentsModule: GenexusModule {
 	override fun initialize(appContext: Context?) {
-		ExternalApiFactory.addApi(ExternalApiDefinition(PaymentsApi.NAME, PaymentsApi::class.java, false))
+		val def = ExternalApiDefinition(PaymentsApi.NAME, PaymentsApi::class.java, false)
+		Services.SuperApps.Api = SuperAppApi(def)
 	}
 }
