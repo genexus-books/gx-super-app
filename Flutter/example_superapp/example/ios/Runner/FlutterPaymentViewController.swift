5eeec1208fa95fbf67a8bcd3c7808b20c2e66cb0
Fabian Inthamoussu <finthamoussu@genexus.com>
2024-09-25 10:50:53 +0200

    getInformation method implementation fixes - Fix indentation

--- Flutter/example_superapp/example/ios/Runner/FlutterPaymentViewController.swift
+++ Flutter/example_superapp/example/ios/Runner/FlutterPaymentViewController.swift
@@ -2,26 +2,26 @@ import Flutter
 import Foundation
 
 class FlutterPaymentViewController: FlutterViewController {
-    
-    private var data: String
-    private var gxResultHandler: ((Result<String, Error>) -> Void)?
+	
+	private var data: String
+	private var gxResultHandler: ((Result<String, Error>) -> Void)?
 	
 	private lazy var flutterChannel = FlutterMethodChannel(name: StaticVars.CHANNEL, binaryMessenger: self.engine!.binaryMessenger)
-
-    init(data: String, gxResultHanlder: @escaping (Result<String, Error>) -> Void) {
-        self.data = data
-        self.gxResultHandler = gxResultHanlder
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            preconditionFailure("unable to obtain AppDelegate")
-        }
-        let newEngine = appDelegate.engines.makeEngine(withEntrypoint: "showPayWithUI", libraryURI: nil)
-        super.init(engine: newEngine, nibName: nil, bundle: nil)
-    }
-
-    required init(coder aDecoder: NSCoder) {
-      fatalError("init(coder:) has not been implemented")
-    }
-    
+	
+	init(data: String, gxResultHanlder: @escaping (Result<String, Error>) -> Void) {
+		self.data = data
+		self.gxResultHandler = gxResultHanlder
+		guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+			preconditionFailure("unable to obtain AppDelegate")
+		}
+		let newEngine = appDelegate.engines.makeEngine(withEntrypoint: "showPayWithUI", libraryURI: nil)
+		super.init(engine: newEngine, nibName: nil, bundle: nil)
+	}
+	
+	required init(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
 	deinit {
 		flutterChannel.setMethodCallHandler(nil)
 		if let gxResultHandler {
@@ -29,42 +29,38 @@ class FlutterPaymentViewController: FlutterViewController {
 		}
 	}
 	
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	override func viewDidLoad() {
+		super.viewDidLoad()
 		flutterChannel.invokeMethod("init", arguments: ["data" : data])
 		flutterChannel.setMethodCallHandler { [weak self] call, result in
-            self?.handleMethodCall(call, result: result)
-        }
-    }
-    
-    private func handleMethodCall(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+			self?.handleMethodCall(call, result: result)
+		}
+	}
+	
+	private func handleMethodCall(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+		guard call.method != "getInformation" else {
+			let args = call.arguments as? [String: Any]
+			guard let data = args?["data"] as? String else {
+				let errDesc = "Invalid \(call.method) arguments: \(String(describing: call.arguments))"
+				result(FlutterError(code: "INVALID_ARGUMENTS", message: errDesc, details: nil))
+				return
+			}
+			switch data {
+			case "session":
+				SuperAppAPI.callMethod(name: SampleExObjHandler.Constants.Methods.GET_SESSION_INFO, arguments: nil, resultHandler: result)
+			default:
+				let errDesc = "Invalid \(call.method) argument: \(data)"
+				result(FlutterError(code: "INVALID_ARGUMENTS", message: errDesc, details: nil))
+			}
+			return
+		}
 		guard let gxResultHandler = self.gxResultHandler else {
 			result(nil)
 			return
 		}
-        if (call.method == "getInformation" ){
-            let args = call.arguments as? [String: Any]
-            guard let data = args?["data"] as? String else {
-                let errDesc = "Invalid result arguments: \(String(describing: call.arguments))"
-                return
-            }
-            switch data {
-                case "session":
-                    self.getSessionInformation { info in
-                        if let info = info {
-                            result(info)
-                        } else {
-                            result(FlutterError(code: "UNAVAILABLE", message: "Data not available.", details: nil))
-                        }
-                    }
-                default:
-                    result(nil)
-            }
-            return
-        }
-        self.gxResultHandler = nil
+		self.gxResultHandler = nil
 		dismiss(animated: true) {
-            defer { result(nil) }
+			defer { result(nil) }
 			switch call.method {
 			case "confirmFlutterActivity":
 				let resultArgs = call.arguments as? [String: Any]
@@ -80,25 +76,9 @@ class FlutterPaymentViewController: FlutterViewController {
 				gxResultHandler(.failure(NSError.defaultGXError(withDeveloperDescription: "Unknown method: \(call.method)")))
 			}
 		}
-    }
-    
-    private func getSessionInformation(completion: @escaping (String?) -> Void) {
-		SuperAppAPI.callMethod(name: SampleExObjHandler.Constants.Methods.GET_SESSION_INFO, arguments: nil) { result in
-			guard let returnValue = result as? String else {
-				let error: NSError
-				if let flutterError = result as? FlutterError {
-					error = NSError.defaultGXError(withDeveloperDescription: flutterError.description)
-				}
-				else {
-					error = NSError.defaultGXError()
-				}
-				return
-			}
-			completion(returnValue)
-		}
 	}
-
-    enum StaticVars {
-        static let CHANNEL = "com.genexus.superapp/paymentConfirm"
-    }
-  }
+	
+	enum StaticVars {
+		static let CHANNEL = "com.genexus.superapp/paymentConfirm"
+	}
+}
